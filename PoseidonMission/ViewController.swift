72a076bd3f8ab719765f3577ab2a601d93955ff5
Amy Yang <kbl26amy@gmail.com>
2019-08-27 09:45:31 +0800

    add firebase kit and establish the base file

---
+++ PoseidonMission/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoseidonMission
+//
+//  Created by 楊雅涵 on 2019/8/26.
+//  Copyright © 2019 AmyYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
