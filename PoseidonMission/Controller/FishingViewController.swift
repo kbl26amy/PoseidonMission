b21a4d851c4fd84bcff2d228b9f40c86bf8ac94e
Amy Yang <kbl26amy@gmail.com>
2019-09-16 00:56:54 +0800

    adjust Fish animation from CAAnimation to PropertyAnimation fix the problem of timer reset (ship) and add a fish view layer

--- PoseidonMission/Controller/FishingViewController.swift
+++ PoseidonMission/Controller/FishingViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class FishingViewController: UIViewController {
 
+    let fishGenerater: [FishGenerator] = [PathOne(), PathTwo(), PathThree(), PathForth()]
+    var timer:Timer?
+ 
+    @IBOutlet weak var fishsView: UIView!
     @IBOutlet weak var ship: UIImageView!
     @IBOutlet weak var fishingButton: UIButton!
     @IBOutlet weak var chanceBagroundView: UIView!
@@ -20,26 +24,144 @@ class FishingViewController: UIViewController {
     @IBOutlet weak var rightMoveButton: UIButton!
     @IBOutlet weak var fishingLine: UIImageView!
     
+    var energyBarAnimator: UIViewPropertyAnimator?
+    
+    let colorView = UIView(frame: CGRect(x:0, y: 0, width: 0, height: UIScreen.main.bounds.height / 26)
+    )
+    
+    @IBAction func backButton(_ sender: Any) {
+        
+        backToRoot()
+    }
+    
+    @IBAction func clickLeftButton(_ sender: Any) {
+        var shipLeftAnimation = UIViewPropertyAnimator()
+        shipLeftAnimation = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, animations: {
+                self.ship.frame.origin.x -= 10
+                self.fishingRod.frame.origin.x -= 10
+                self.fishingLine.frame.origin.x -= 10
+                }, completion: nil)
+        shipLeftAnimation.startAnimation()
+    }
+    
+    @IBAction func clickRightButton(_ sender: Any) {
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, animations: {
+            self.ship.frame.origin.x += 10
+            self.fishingRod.frame.origin.x += 10
+            self.fishingLine.frame.origin.x += 10
+        }, completion: nil)
+        
+    }
+    @IBAction func clickFishingButton(_ sender: UIButton) {
+        
+         self.energyBarAnimator?.stopAnimation(true)
+        
+        if self.colorView.frame.width >= self.energyBar.frame.width * 0.95 {
+            
+            let sucessText = UILabel(frame: self.colorView.frame)
+            sucessText.textColor = .white
+            sucessText.text = "能量充足"
+            sucessText.textAlignment = .center
+            self.colorView.addSubview(sucessText)
+            rotateUpRod(sender)
+            
+        } else {
+            self.colorView.frame = CGRect(x:0, y: 0, width: 0, height: UIScreen.main.bounds.height / 26)
+            
+            let alertController = UIAlertController(title: "失敗", message: "能量條不足", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default) { (_) in
+                
+                self.energyBarAnimation()
+                
+                }
+      
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+ 
+        }
+ 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.isNavigationBarHidden = true
         tabBarController?.tabBar.isHidden = true
+        setFishingView()
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        energyBarAnimation()
+        self.timerEanbled()
+
+    }
+    
+    func energyBarAnimation() {
+     
+        colorView.backgroundColor = .red
+        
+        self.energyBar.addSubview(colorView)
+        
+        energyBarAnimator = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 2.0, delay: 0, options: .repeat, animations: {
+            UIView.setAnimationRepeatCount(999);
+            self.colorView.frame = CGRect(x:0, y: 0, width: self.energyBar.frame.width, height: self.energyBar.frame.height)
+            
+        }, completion: nil)
+        
+        energyBarAnimator?.startAnimation()
+    }
+    
+    func setFishingView() {
         
         ship.image = UIImage(named: "ship")
         chanceBagroundView.layer.cornerRadius = 15
         fishingButton.layer.cornerRadius = 15
         fishingBackground.image = UIImage(named: "background")
         fishingRod.image = UIImage(named: "fishingRod")
-        
         fishingLine.image = UIImage(named: "fishingLine")
         rightMoveButton.setImage(UIImage(named: "rightButton"), for: .normal)
         leftMoveButton.setImage(UIImage(named: "leftButton"), for: .normal)
         energyBar.layer.cornerRadius = 15
         energyBar.layer.borderWidth = 3
         energyBar.layer.borderColor = #colorLiteral(red: 0.1359939873, green: 0.3958500326, blue: 0.4860774279, alpha: 1)
-  
     }
     
-
+    func rotateUpRod(_ sender: Any) {
+  
+        var rodUpAnimation: UIViewPropertyAnimator?
+        var rodDownAnimation: UIViewPropertyAnimator?
+        
+        rodUpAnimation =  UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.5, delay: 0, animations: {
+            
+            self.fishingRod.transform = CGAffineTransform(rotationAngle: (20.0 * .pi) / 90.0)
+ 
+        }, completion: nil)
+        rodUpAnimation?.startAnimation()
+        rodUpAnimation?.addCompletion() {_ in
+            rodDownAnimation = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.5, delay: 0, animations: {
+//                self.fishingRod.frame.origin.y += 20
+                self.fishingRod.transform = CGAffineTransform(rotationAngle: (-20.0 * .pi) / 90.0)
+            }, completion: nil)
+            
+            rodDownAnimation?.startAnimation()
+        }
+    }
+    
+    func timerEanbled(){
+        
+        self.timer = Timer.scheduledTimer(timeInterval: 2,
+                                          target: self,
+                                          selector: #selector(generateFish),
+                                          userInfo: nil, repeats: true)
+    }
+    
+    @objc func generateFish(){
+        fishsView.addSubview(fishGenerater.randomElement()!.fetchFishImageView())
+    }
+            
 }
---
+++ PoseidonMission/Model/FishManager.swift
@@ -0,0 +1,96 @@
+//
+//  FishManager.swift
+//  PoseidonMission
+//
+//  Created by 楊雅涵 on 2019/9/13.
+//  Copyright © 2019 AmyYang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol FishGenerator {
+    
+    func fetchFishImageView() -> UIImageView
+}
+
+extension FishGenerator {
+    
+    func randomFishImage() -> UIImageView? {
+        
+        let fishs = ["fish1", "fish2", "fish3", "fish4", "fish5", "fish6", "fish7", "fish8", "fish9"]
+        
+        let fishImageView = UIImageView(image:  UIImage(named: fishs.randomElement()!))
+        fishImageView.frame = CGRect(x: 0, y: 0, width: 60, height: 40)
+        
+        return fishImageView
+    }
+}
+
+struct PathOne: FishGenerator {
+    
+    func fetchFishImageView() -> UIImageView {
+        let fish = randomFishImage()
+        fish?.frame = CGRect(x: 0, y: 300, width: 60, height: 40)
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 11, delay: 0, animations: {
+            fish?.frame.origin.x = UIScreen.main.bounds.size.width
+         
+            fish?.frame.origin.y = UIScreen.main.bounds.size.height / 2
+            
+        }, completion: nil)
+     
+        return fish!
+    }
+}
+
+
+struct PathTwo: FishGenerator {
+    
+    func fetchFishImageView() -> UIImageView {
+       let fish = randomFishImage()
+        
+        fish?.frame = CGRect(x: 0, y: 450, width: 60, height: 40)
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 12, delay: 0, animations: {
+            fish?.frame.origin.x = UIScreen.main.bounds.size.width
+            
+            fish?.frame.origin.y = UIScreen.main.bounds.size.height / 4
+            
+        }, completion: nil)
+        
+        return fish!
+    }
+}
+
+struct PathThree: FishGenerator {
+    
+    func fetchFishImageView() -> UIImageView {
+       let fish = randomFishImage()
+        
+        fish?.frame = CGRect(x: 0, y: 200, width: 60, height: 40)
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 10, delay: 0, animations: {
+            fish?.frame.origin.x = UIScreen.main.bounds.size.width
+            
+            fish?.frame.origin.y = UIScreen.main.bounds.size.height / 7
+            
+        }, completion: nil)
+        
+        return fish!
+    }
+}
+
+struct PathForth: FishGenerator {
+    
+    func fetchFishImageView() -> UIImageView {
+        let fish = randomFishImage()
+        
+        fish?.frame = CGRect(x: 0, y: 50, width: 60, height: 40)
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 15, delay: 0, animations: {
+            fish?.frame.origin.x = UIScreen.main.bounds.size.width
+            
+            fish?.frame.origin.y = UIScreen.main.bounds.size.height - 400
+            
+        }, completion: nil)
+        
+        return fish!
+    }
+}
